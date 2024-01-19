dcab6f942ab887fec417cd8045c1de5e2efcb03c
poissonfish <niche@vt.edu>
2024-01-19 04:53:09 -0500

    pub all sets to hf

--- data/3_pub_hf.py
+++ data/3_pub_hf.py
@@ -9,6 +9,6 @@ from datasets import load_dataset
 PATH = os.path.join("/Users", "niche", "_03_Papers", "2024", "cowsformer", "data")
 os.chdir(PATH)
 setnames = ["1a_angle_t2s", "1b_angle_s2t", "2_light", "3_breed", "4_all"]
-setname = setnames[0]
-dataset = load_dataset("config.py", setname)
-dataset.push_to_hub("Niche-Squad/cowsformer", setname)
+for setname in setnames:
+    dataset = load_dataset("config.py", setname)
+    dataset.push_to_hub("Niche-Squad/cowsformer", setname)
