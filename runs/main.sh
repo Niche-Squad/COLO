bc5aff52e9d026ded0c065a7f451a7f0c85f9d73
poissonfish <niche@vt.edu>
2024-03-12 06:11:38 -0400

    add job titles

--- runs/main.sh
+++ runs/main.sh
@@ -4,20 +4,20 @@
 sbatch runs/init_data.sh data
 
 # pre-train models
-sbatch runs/pretrain.sh 0_all
-sbatch runs/pretrain.sh 1_top
-sbatch runs/pretrain.sh 2_side
+sbatch --job-name=pre-all runs/pretrain.sh 0_all
+sbatch --job-name=pre-top runs/pretrain.sh 1_top
+sbatch --job-name=pre-side runs/pretrain.sh 2_side
 
 # generalization test - COCO pre-trained models
 for config in "0_all" "a1_t2s" "a2_s2t" "b_light" "c_external"
 do
-    sbatch runs/yolo_coco.sh $config
+    sbatch --job-name=${config} runs/yolo_coco.sh $config
 done
 
 # generalization test - custom pre-trained models
 for config in "1_top" "2_side" "3_external"
 do
-    sbatch runs/yolo_custom.sh $config
+    sbatch --job-name=${config} runs/yolo_custom.sh $config
 done
 
 
